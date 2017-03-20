b15af72d8c294b4890fbd85965d5124e098c625c
salty-vanilla <jacketgardensio@gmail.com>
2017-03-21 00:50:02 +0900

    fix modules

--- interface.py
+++ interface.py
@@ -4,7 +4,6 @@ from keras.layers import (
 )
 from keras import backend as K
 from keras.models import Model
-import os
 import numpy as np
 
 
@@ -12,16 +11,11 @@ class Siamese:
     def __init__(self, input_shape, base_network, param_path_base_network=None):
         self.base_network = base_network
         self.input_shape = input_shape
-        self.siamese_network = None
+        self.network = None
         self.param_path_base_network = param_path_base_network
-        self.trained = False
+        self.build()
 
-        if param_path_base_network and os.path.exists(param_path_base_network):
-            self.trained = True
-        else:
-            self.param_path_base_network = None
-
-    def build(self, optimizer):
+    def build(self):
         input_a = Input(shape=self.input_shape)
         input_b = Input(shape=self.input_shape)
 
@@ -34,35 +28,14 @@ class Siamese:
         distance = Lambda(self.euclidean_distance,
                           output_shape=self.eucl_dist_output_shape)([processed_a, processed_b])
 
-        self.siamese_network = Model([input_a, input_b], distance)
-        self.siamese_network.compile(loss=self.contrastive_loss,
-                                     optimizer=optimizer)
+        self.network = Model([input_a, input_b], distance)
 
-    def fit(self, x_pairs_train, is_matched_train,
-            x_pairs_valid=None, is_matched_valid=None,
-            batch_size=32, epochs=100,
-            param_path="./params/siamese.hdf5", callbacks=None):
-
-        if x_pairs_valid and is_matched_valid:
-            self.siamese_network.fit([x_pairs_train[:, 0], x_pairs_train[:, 1]], is_matched_train,
-                                     validation_data=([x_pairs_valid[:, 0], x_pairs_valid[:, 1]], is_matched_valid),
-                                     batch_size=batch_size, epochs=epochs, callbacks=callbacks)
-        else:
-            self.siamese_network.fit([x_pairs_train[:, 0], x_pairs_train[:, 1]], is_matched_train,
-                                     batch_size=batch_size, epochs=epochs, callbacks=callbacks)
-
-        os.makedirs(os.path.split(param_path)[0], exist_ok=True)
-        self.siamese_network.save_weights(param_path)
-
-        self.trained = True
+    def compile(self, optimizer):
+        # self.build()
+        self.network.compile(loss=self.contrastive_loss, optimizer=optimizer)
 
     def predict(self, x_pairs):
-        if self.trained and not self.param_path_base_network:
-            pass
-        else:
-            self.base_network.load_weights(self.param_path_base_network)
-
-        predictions = self.siamese_network.predict([x_pairs[:, 0], x_pairs[:, 1]])
+        predictions = self.network.predict([x_pairs[:, 0], x_pairs[:, 1]])
         return predictions
 
     def eval(self, x_pairs, is_matched, thr=0.5):
@@ -78,7 +51,9 @@ class Siamese:
     @staticmethod
     def eucl_dist_output_shape(shapes):
         shape1, shape2 = shapes
-        return (shape1[0], 1)
+        bs = shape1[0]
+        shape = (bs, 1)
+        return shape
 
     @staticmethod
     def contrastive_loss(y_true, y_pred):
